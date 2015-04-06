98640896532e27f57ff9f76a1b8acc54cf8de2e4
Harold Valdivia Garcia <harold.valdivia@upr.edu>
2015-04-06 17:10:07 -0400

    Add sql-date

--- pysilisk/types.py
+++ pysilisk/types.py
@@ -23,3 +23,29 @@ class DateTime(datetime):
         self = super(DateTime,cls).fromtimestamp(t)
         self._timestamp = t
         return self
+
+
+class Date(date):
+    """Represents the SQL type "DATE"
+    Date is stored as a 4-byte integer using the following
+    format:
+        date_as_int = day + 100*month + 10000*year
+
+    To convert to bytes:
+        buffer = struct.pack("<i", dt.to_int4())
+
+    To convert from bytes:
+        f = struct.unpack("<i", buffer)[0]
+        dt = Date.from_int4(f)
+    """
+
+    @property
+    def to_int4(self):
+        return self.day + self.month*100 + self.year*10000
+
+    @classmethod
+    def from_int4(cls, date_as_int):
+        year = date_as_int // 10000
+        month = date_as_int % 10000 // 100
+        day = date_as_int % 100
+        return cls(year, month, day)
