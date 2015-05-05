738ff9802c25e51f192f9dcf6a492140515e3c27
Harold Valdivia Garcia <harold.valdivia@upr.edu>
2015-05-04 22:04:38 -0400

    Improve cli and server skeletons

--- pysilisk/cli/minicli.py
+++ pysilisk/cli/minicli.py
@@ -3,6 +3,7 @@ import re
 import os
 import glob
 import argparse
+from pysilisk.server import PysiliskSQL
 
 # Logging Configuration
 logger = logging.getLogger()
@@ -11,49 +12,39 @@ logging.basicConfig(level=logging.INFO, format=logformat)
 logging.basicConfig(level=logging.INFO)
 
 
-class PysiliskSQL:
-    pass
+class MetaCMD(object):
+    """
+    Meta-commands always begin with "\". These commands are implemented
+    in the cli-component.
+        \h        : help.',
+        \q        : quit the pysilik console
+        \sd       : show databases.
+        \c dbname : connect to dbname
+                    create a new db if dbname does not exist
+    """
+    QUIT = '\\q'
+    HELP = '\\h'
+    SHOW_DBs = '\\sd'
+    CONNECT_DB = r'^\\c \w+$'
+    @classmethod
+    def meta_cmd_is_valid(cls, meta_cmd):
+        """Check if it is a valid-meta-command"""
+        if meta_cmd in [cls.HELP, cls.SHOW_DBs, cls.QUIT]:
+            return True
+        if re.compile(cls.CONNECT_DB, meta_cmd):
+            return  True
+        return False
 
-class Connection(object):
-    def __init__(self, db_path, buffer_size):
-        self.db_path = db_path
-        self.db_name = db_path.strip('/').split('/')[-1]
-
-        # Open a direct access to pysilisk
-        self.pysilisk_server = PysiliskSQL(self.db_path, buffer_size)
-        if not os.path.exists(self.db_path):
-            self.pysilisk_server.create_db()
-        self.pysilisk_server.open_db()
-
-    def execute(self, sql_str):
-        pass
-
-    def close(self):
-        pass
-
-    def has_resultset(self):
-        pass
-
-    def resultset(self):
-        pass
-
-    @property
-    def affected_rows(self):
-        pass
-
-    @property
-    def execution_time(self):
-        pass
 
 class SimpleCli:
     """A simple REPL (Read–Eval–Print Loop) to submit commands to PysiliskSQL"""
     def __init__(self):
         self.data_directory = ""
-        self.db_conn = Connection()
+        self.pysilisk_server = PysiliskSQL()
 
     @property
     def prompt(self):
-        return "%s>" % self.db_conn.db_name
+        return "%s>" % self.pysilisk_server.db_name
 
     def run_cli(self):
         """REPL (Read–Eval–Print Loop) to submit commands to PysiliskSQL"""
@@ -61,8 +52,8 @@ class SimpleCli:
             str_cmd = input(self.prompt).strip()
             if str_cmd.startswith('\\'):
                 # Process meta-commands
-                if str_cmd == '\\q':
-                    self.db_conn.close()
+                if str_cmd == MetaCMD.QUIT:
+                    self.pysilisk_server.close()
                     break
                 self.process_meta_command(str_cmd)
             elif str_cmd != '':
@@ -71,32 +62,30 @@ class SimpleCli:
         print('Bye, bye ...')
 
     def process_meta_command(self, str_cmd):
-        if meta_cmd_is_valid(str_cmd):
-            if str_cmd == '\\h':
+        if MetaCMD.meta_cmd_is_valid(str_cmd):
+            if str_cmd == MetaCMD.HELP:
                 help_cmd()
-            elif re.match(r'^\\c \w+$', str_cmd):
-                self.db_conn.close()
+            elif re.match(MetaCMD.CONNECT_DB, str_cmd):
+                self.pysilisk_server.close()
                 # Open a new connection
-                db_name = str_cmd.split()[-1]
+                cmd, db_name = str_cmd.split()  # str_cmmd = "\c dbname"
                 db_path = os.path.join(self.data_directory, db_name)
-                self.db_conn = create_connection(db_path)
+                self.pysilisk_server = create_pysilisk_server(db_path)
                 print('You are now connected to database "%s"' % db_name)
-            elif str_cmd == '\\sd':
-                # Show the databases
-                db_dirs = glob.glob(os.path.join(self.data_directory,'*/'))
-                for dir in db_dirs:
-                    db_name = dir.strip('/').split("/")[-1]
+            elif str_cmd == MetaCMD.SHOW_DBs:
+                # Show the databases in the data-dir
+                for db_name in list_subdirs(self.data_directory):
                     print(db_name)
         else:
-            print('Invalid command "%s". Try \h for help.'%str_cmd)
+            print('Invalid command "%s". Try \\h for help.'%str_cmd)
 
     def process_sql_query(self, str_query):
-        self.db_conn.execute(str_query)
-        if self.db_conn.has_resultset():
-            print_rs(self.db_conn.resultset())
+        self.pysilisk_server.execute(str_query)
+        if self.pysilisk_server.has_resultset():
+            print_rs(self.pysilisk_server.resultset())
         else:
-            print('Affected rows: %s' % self.db_conn.affected_rows)
-            print('Execution time: %s' % self.db_conn.execution_time)
+            print('Affected rows: %s' % self.pysilisk_server.affected_rows)
+            print('Execution time: %s' % self.pysilisk_server.execution_time)
 
 
 def print_rs(result_set):
@@ -116,32 +105,29 @@ def help_cmd():
                 '       \\q        : quit the pysilik console.',
                 '       \\sd       : show databases.',
                 '       \\c dbname : connect to dbname.',
-                '                    create a new db if dbname doesn\'t exist')
+                '                    create a new db if dbname does not exist')
     help_txt = ['%s' % s for s in help_txt]
     print('\n'.join(help_txt))
 
 
-def meta_cmd_is_valid(meta_cmd):
-    """Check if it is a valid-meta-command"""
-    if meta_cmd == '\\h' or meta_cmd == '\\sd' or meta_cmd == '\\q':
-        return True
-    if re.compile(r'^\\c \w+$', meta_cmd):
-        return  True
-    return False
-
-
-def create_connection(db_path):
-    conn = Connection(db_path=db_path)
-    return conn
+def list_subdirs(root_dir):
+    fullpath_dirs = glob.glob(os.path.join(root_dir,'*/'))
+    sub_dirs = []
+    for dir in fullpath_dirs:
+        subdir = dir.strip('/').split("/")[-1]
+        sub_dirs.append(subdir)
+    return sub_dirs
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--pysilisk-dir',
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--pysilisk-dir',
                         metavar='DIRECTORYPATH',
                         required=True,
                         help="set the data-directory of PysiliskSQL")
-    args = parser.parse_args()
+    args = arg_parser.parse_args()
+
     # Run the pysilisk-shell
     cmd_line = SimpleCli(data_directory=args.pysilisk_dir)
     cmd_line.run_cli()
--- pysilisk/server.py
+++ pysilisk/server.py
@@ -1,3 +1,6 @@
+import os
+
+
 class DDLCompiler(object):
     pass
 
@@ -26,6 +29,7 @@ class RecordManager(object):
 class ResultSet(object):
     def __init__(self, physical_plan):
         self._physical_plan = physical_plan
+        self._physical_plan.open()
         self._buffer_tuples = []
         self._size_buffer = len(self.buffer_tuples)
         self._idx_next == 0
@@ -38,6 +42,7 @@ class ResultSet(object):
             self._buffer_tuples = self._physical_plan.next()
             self._size_buffer = len(self._buffer_tuples)
             if self._size_buffer == 0:
+                self._physical_plan.close()
                 raise StopIteration()
             self._idx_next = 0
         next_row = self._buffer_tuples[self._idx_next]
@@ -45,13 +50,15 @@ class ResultSet(object):
         return next_row
 
 
-# http://en.wikibooks.org/wiki/Python_Programming/Source_Documentation_and_Comments
-# http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
-# http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
+def create_pysilisk_server(db_path):
+    pass
 
-class PysiliskDB:
-    ddl_compiler = DDLCompiler()
-    def __init__(self):
+
+class PysiliskSQL:
+    """Facade component"""
+
+    def __init__(self, db_directory_path):
+        self.db_directory_path = db_directory_path
         self.ddl_compiler = DDLCompiler()
         self.query_compiler = QueryCompiler()
         self.engine = ExecutionEngine()
@@ -59,24 +66,85 @@ class PysiliskDB:
         self.sql_preprocessor = 1
         self.query_preproc = 1
         self.sql_preprocsr = 1
+        self.result_set = None
+        self.ddl_manager = None
+        self.parser = None
+        self.optimizer = None
 
     def execute(self, sql_str):
-        parse_tree = self.parser(sql_str)
-        # semantic check with the query_preprocessor
-        annotate_tree = self.sql_preprocsr.check_and_annotate_tree(parse_tree)
-        if parse_tree.type == self.parser.DDL_STMT_TYPE:
-            #
+        # Create and check the ast-tree that represent the query
+        query_tree = self.parser.parse_query(sql_str)
+        checked_query_tree = self.sql_preprocsr.semantic_check(query_tree)
+
+        if not checked_query_tree.is_dml_stmt():
+            physical_plan = self.ddl_manager.evaluate(checked_query_tree)
+        else:
+            # Create a logical-plan | plan-rewriter| do basic transformations
+            # create a physical-plan using the optimizer
+            # Create a wrapper of the physical-plan that
+            # simulate the results-of-the-query
+            #: Optimize is a better word but I use evaluate because ddlmanager
+            #: uses it
+            physical_plan = self.optimizer.evaluate(checked_query_tree)
+
+        result = physical_plan.execute()
+        if isinstance(result, int):
+            self.affected_rows = result
+            self.result_set = None
+        elif isinstance(result, ResultSet):
+            self.result_set = result
             self.affected_rows = -1
-        if parse_tree.type == self.parser.DML_STMT_TYPE:
-            pass
-            # Create a logical-plan
-            # create a phisical-plan using an optimizer
-            # Create a wrapper of the physical-plan that simulate the results-of-the-query
-            physical_plan = None
-            self._resultset = ResultSet(physical_plan)
 
-
-
+    def open(self):
+        if not os.path.exists(self.db_directory_path):
+            self.pysilisk_server.create_db()
+        self.pysilisk_server.open_db()
 
     def close(self):
         pass
+
+
+
+# Notes:
+# =========================
+# http://en.wikibooks.org/wiki/Python_Programming/Source_Documentation_and_Comments
+# http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
+# http://thomas-cokelaer.info/tutorials/sphinx/docstring_python.html
+
+# Note: A physical-plan is not an iterator
+# We use two different concepts: relational-operators(iterators)
+# and Plans. A plan can wrap a set of rel-operators or DDL-code
+
+
+# Purdue professor: clifton
+# =========================
+# Minisql-mini in Java:
+#https://www.cs.purdue.edu/homes/clifton/cs541/project4/javadocs/index.html?query/Insert.html
+# CS541 Spring12 - Project 4: Query Evaluation and Optimization
+#https://www.cs.purdue.edu/homes/clifton/cs541/project4/project4.zip
+#https://www.cs.purdue.edu/homes/clifton/cs541/project2/
+#https://www.cs.purdue.edu/homes/clifton/cs541/project4/
+
+
+# Original MINI just doc
+# =========================
+# http://research.cs.wisc.edu/coral/minibase/
+
+
+# Berkeley:
+# =========================
+#https://inst.eecs.berkeley.edu/~cs186/sp07/homework/hw1/hw1.html
+# https://inst.eecs.berkeley.edu/~cs186/sp07/homework/hw2/hw2.html
+#https://inst.eecs.berkeley.edu/~cs186/sp07/projects.html
+
+
+# Portland:
+# =========================
+# http://web.cecs.pdx.edu/~tufte/dbimplem-spr2014/
+#http://www.cse.unsw.edu.au/~cs9315/14s2/notes/G/notes.html
+#http://cgi.cse.unsw.edu.au/~cs9315/14s2/index.php
+
+# =========================
+# http://www.cse.unsw.edu.au/~cs9315/14s2/notes/A/notes.html
+
+# =======================================================================
